fe3914b926888de8627ccddbe867dea8a5614dc2
1289746201 <yuethanhaonan@outlook.com>
2023-03-15 10:39:08 +0800

    基础覆盖

---
+++ 11组件化开发/index.vue
@@ -0,0 +1,26 @@
+<template id="component-b">
+  <div>
+    <h2>title2</h2>
+    <p>this is content2</p>
+    <button @click="btnClick"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "I am title",
+      contents: "I am content",
+    };
+  },
+  methods: {
+    btnClick() {
+      console.log("dianjikaifa");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
